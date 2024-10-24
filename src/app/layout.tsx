e83ea3aa752919d7b4c4f7612e24dc0c55c23700
StoneZol <miha2000rus@gmail.com>
2024-10-24 12:46:26 +0300

    попытка подправить слой

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({children} : Readonly < {
                 {/* Open Graph (OG) мета-теги */}
                 <meta property="og:title" content="StoneZol"/>
                 <meta property="og:description" content="Обо мне"/>
-                <meta property="og:image" content="/"/>
+                <meta property="og:image" content="/src/shared/Static/avatars/b3.webp"/>
                 <meta property="og:url" content="https://visitka-next.vercel.app/"/>
                 <meta property="og:type" content="website"/>
 
